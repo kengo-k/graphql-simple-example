d34f7222a4a75807d9cc70c97f92a068cb19451d
kengo-k <kurobanekengo@gmail.com>
2022-10-05 23:03:40 +0900

    queryを詳細化

--- index.ts
+++ index.ts
@@ -1,14 +1,64 @@
 import { ApolloServer, gql } from 'apollo-server'
 
+interface Category {
+  id: number
+  name: string
+}
+
+interface Task {
+  id: number
+  title: string
+  category: Category
+  priority: number
+  done: boolean
+}
+
 const typeDefs = gql`
+  type Category {
+    id: ID
+    name: String
+  }
+
+  type Task {
+    id: ID
+    title: String
+    category: Category
+    priority: Int
+    done: Boolean
+  }
+
   type Query {
-    hello: String
+    tasks: [Task]
   }
 `
 
+const allTasks: Task[] = [
+  {
+    id: 1,
+    title: 'title1',
+    category: { id: 1, name: 'category1' },
+    priority: 10,
+    done: false,
+  },
+  {
+    id: 2,
+    title: 'title2',
+    category: { id: 1, name: 'category1' },
+    priority: 7,
+    done: false,
+  },
+  {
+    id: 3,
+    title: 'title3',
+    category: { id: 2, name: 'category2' },
+    priority: 5,
+    done: false,
+  },
+]
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello',
+    tasks: () => allTasks,
   },
 }
 
