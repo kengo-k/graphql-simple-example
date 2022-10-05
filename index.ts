94b9a6e32faaf85fe539dadc93dc265528e9a4c9
kengo-k <kurobanekengo@gmail.com>
2022-10-06 00:20:38 +0900

    index.ts: 切り出したserverを使うように修正

--- index.ts
+++ index.ts
@@ -1,93 +1,9 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { init } from './src/server'
 
-interface Category {
-  id: number
-  name: string
-}
-
-interface Task {
-  id: number
-  title: string
-  category: Category
-  priority: number
-  done: boolean
-}
-
-const typeDefs = gql`
-  type Category {
-    id: ID
-    name: String
-  }
-
-  type Task {
-    id: ID
-    title: String
-    category: Category
-    priority: Int
-    done: Boolean
-  }
-
-  type Query {
-    all: [Task]
-    findByCategory(category: Int): [Task]
-  }
-`
-
-const resolvers = {
-  Query: {
-    all: () => allTasks,
-    findByCategory: (_: any, args: any) => {
-      return allTasks.filter((task) => {
-        return task.category.id === args.category
-      })
-    },
-  },
-}
-
-const allTasks: Task[] = [
-  {
-    id: 1,
-    title: 'title1',
-    category: { id: 1, name: 'category1' },
-    priority: 10,
-    done: false,
-  },
-  {
-    id: 2,
-    title: 'title2',
-    category: { id: 1, name: 'category1' },
-    priority: 7,
-    done: false,
-  },
-  {
-    id: 3,
-    title: 'title3',
-    category: { id: 2, name: 'category2' },
-    priority: 5,
-    done: false,
-  },
-]
-
-function init() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  })
-
-  return {
-    start: async () => {
-      const { url } = await server.listen()
-      console.log(`🚀  Server ready at ${url}`)
-    },
-    stop: () => {
-      server.stop
-    },
-  }
-}
+const { start } = init()
 
 async function main() {
-  const { start } = init()
-  start()
+  await start()
 }
 
 main()
