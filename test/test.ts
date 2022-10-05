af356045bbbb07a17436336d4ab3c8aeff359051
kengo-k <kurobanekengo@gmail.com>
2022-10-06 00:21:27 +0900

    test: beforeとafter追加

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,16 @@
+import { init } from '../src/server'
+
+const { start, stop } = init()
+beforeAll(async () => {
+  console.log('> start server...')
+  await start()
+})
+
+afterAll(async () => {
+  console.log('> stop server...')
+  await stop()
+})
+
 test('1 = 1', function () {
   expect(1).toBe(1)
 })
