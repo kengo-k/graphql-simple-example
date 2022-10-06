d1f0bab6688064c5ac2f188c4a7c5f4ed604dd26
kengo-k <kurobanekengo@gmail.com>
2022-10-06 16:12:18 +0900

    mutationテストを追加

--- test/test.ts
+++ test/test.ts
@@ -40,3 +40,73 @@ test('all', async function () {
     ],
   })
 })
+
+test('update task title', async function () {
+  const response = await axios.post('http://localhost:4000', {
+    query: `
+        mutation {
+          updateTask(id: 1, task: {title: "title999"}) {
+            id
+            title
+          }
+        }`,
+  })
+  expect(response.data.data).toMatchObject({
+    updateTask: {
+      id: '1',
+      title: 'title999',
+    },
+  })
+})
+
+test('update task title with varialbes', async function () {
+  const response = await axios.post('http://localhost:4000', {
+    query: `
+        mutation($id: ID, $task: TaskCondition) {
+          updateTask(id: $id, task: $task) {
+            id
+            title
+          }
+        }`,
+    variables: {
+      id: 1,
+      task: {
+        title: 'title998',
+      },
+    },
+  })
+  expect(response.data.data).toMatchObject({
+    updateTask: {
+      id: '1',
+      title: 'title998',
+    },
+  })
+})
+
+test('all(2nd)', async function () {
+  const response = await axios.post('http://localhost:4000', {
+    query: `
+        query {
+          all {
+            id
+            title
+          }
+        }`,
+  })
+  expect(response.data.data).toMatchObject({
+    all: [
+      {
+        id: '1',
+        title: 'title998',
+      },
+      {
+        id: '2',
+        title: 'title2',
+      },
+      {
+        id: '3',
+        title: 'title3',
+      },
+    ],
+  })
+})
