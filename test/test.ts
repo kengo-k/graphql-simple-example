4553b49ed74f7b52d1a89b25d01b92b1c000e1ab
kengo-k <kurobanekengo@gmail.com>
2022-10-06 14:25:40 +0900

    test: axiosによるテストケースを追加

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,7 @@
 import { init } from '../src/server'
 
+const axios = require('axios')
+
 const { start, stop } = init()
 beforeAll(async () => {
   console.log('> start server...')
@@ -11,6 +13,30 @@ afterAll(async () => {
   await stop()
 })
 
-test('1 = 1', function () {
-  expect(1).toBe(1)
+test('1 + 1 = 2', function () {
+  expect(1 + 1).toBe(2)
+})
+
+test('all', async function () {
+  const response = await axios.post('http://localhost:4000', {
+    query: `
+        query {
+          all {
+            id
+          }
+        }`,
+  })
+  expect(response.data.data).toMatchObject({
+    all: [
+      {
+        id: '1',
+      },
+      {
+        id: '2',
+      },
+      {
+        id: '3',
+      },
+    ],
+  })
 })
