5cf5dc427f991c35b55a82587f5acf4b99777dd1
kengo-k <kurobanekengo@gmail.com>
2022-10-06 14:21:51 +0900

    test.ts: 疎通用テストからheaderを削除(不要だったので)

--- test.ts
+++ test.ts
@@ -5,22 +5,14 @@ const axios = require('axios')
 const { start, stop } = init()
 async function main() {
   await start()
-  const response = await axios.post(
-    'http://localhost:4000',
-    {
-      query: `
-        query xxx {
+  const response = await axios.post('http://localhost:4000', {
+    query: `
+        query {
           all {
             id
           }
         }`,
-    },
-    {
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
-  )
+  })
   console.log(JSON.stringify(response.data.data))
   await stop()
 }
